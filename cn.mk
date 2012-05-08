7c6395c82d1b85b88ed5d58d7997aafd919f49ec
Tommy Nguyen <tommy@p150hm.(none)>
2012-05-08 08:55:17 -0400

    Change version name to sinatra

--- cn.mk
+++ cn.mk
@@ -15,17 +15,17 @@ PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_supersonic BUILD_FINGERPRINT=sprint/htc_supersonic/supersonic:2.3.5/GRJ90/239674.3:user/release-keys PRIVATE_BUILD_DESC="4.67.651.3 CL239674 release-keys"
 
 # Set up the product codename, build version & MOTD.
-PRODUCT_CODENAME := Butta
+PRODUCT_CODENAME := Sinatra
 PRODUCT_VERSION_DEVICE_SPECIFIC := 1
 
-PRODUCT_MOTD :="Thank you for choosing classicnerd for you android experience.  You can follow us at www.classicnerd.net"
+PRODUCT_MOTD :="Thank you for choosing classicnerd for you android experience.  \nYou can follow us at www.classicnerd.net. \nEnjoy the possbilities..."
 
 PRODUCT_PACKAGES += \
     Camera
 
 # Hot reboot
 PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/ev/overlay/hot_reboot
+    vendor/cn/overlay/hot_reboot
 
 # Copy compatible prebuilt files
 PRODUCT_COPY_FILES +=  \
